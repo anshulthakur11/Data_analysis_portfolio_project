5834cd2cc250051121b4b181dddc61900671ec35
anshulthakur11 <140435187+anshulthakur11@users.noreply.github.com>
2024-02-01 00:06:19 +0530

    Update SQLQuery2.sql

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -9,6 +9,42 @@ WITH hotels AS (
     FROM dbo.[hotel_revenue_historical_full-2 2020]
 )
 
+SELECT COALESCE(stays_in_week_nights, 0) + COALESCE(stays_in_weekend_nights, 0) AS total_stays
+FROM hotels;
+
+WITH hotels AS (
+    SELECT *
+    FROM dbo.[hotel_revenue_historical_full-2 2019]
+    UNION
+    SELECT *
+    FROM dbo.[hotel_revenue_historical_full-2 2018]
+    UNION
+    SELECT *
+    FROM dbo.[hotel_revenue_historical_full-2 2020]
+)
+
+SELECT
+    arrival_date_year,
+    COALESCE(stays_in_week_nights, 0) + COALESCE(stays_in_weekend_nights, 0) AS total_stays,
+    SUM((COALESCE(stays_in_week_nights, 0) + COALESCE(stays_in_weekend_nights, 0)) * CAST(adr AS DECIMAL(10, 2))) AS total_revenue
+FROM
+    hotels
+GROUP BY
+    arrival_date_year;
+
+
+
+WITH hotels AS (
+    SELECT *
+    FROM dbo.[hotel_revenue_historical_full-2 2019]
+    UNION
+    SELECT *
+    FROM dbo.[hotel_revenue_historical_full-2 2018]
+    UNION
+    SELECT *
+    FROM dbo.[hotel_revenue_historical_full-2 2020]
+)
+
 SELECT * from hotels
 left join dbo.[hotel_revenue_historical_full-2 market_segment]
 on hotels.market_segment=[hotel_revenue_historical_full-2 market_segment].market_segment
